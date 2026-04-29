8e1f5ba0e85982c1ad05389043175daed5f2ff6c
rahul_v <rahulv1904@gmail.com>
2026-04-29 13:24:46 +0530

    Python basis

---
+++ Even_Odd.py
@@ -0,0 +1,15 @@
+
+
+
+def even_odd(num):
+    '''
+    This checks a number is even or odd
+    parmas : int (n)
+
+    return : print statemens 
+    '''
+    if num % 2 == 0 :
+        print(f'{num} is even number')
+    else:
+        print(f'{num} is Odd Number')
+
